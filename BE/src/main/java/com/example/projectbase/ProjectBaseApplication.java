6f02691c742f98bc906592717ee19e68b13e107c
hoa.dotrung <hoa.dotrung@vti.com.vn>
2025-05-10 09:26:37 +0700

    feat: Un hide login and user API

--- BE/src/main/java/com/example/projectbase/ProjectBaseApplication.java
+++ BE/src/main/java/com/example/projectbase/ProjectBaseApplication.java
@@ -10,6 +10,10 @@ import com.example.projectbase.service.DataCrawlingService;
 import com.example.projectbase.service.impl.DataCrawlingServiceImpl;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+
+import java.io.IOException;
+import java.util.Locale;
+
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -21,49 +25,72 @@ import org.springframework.web.client.RestTemplate;
 
 @Slf4j
 @RequiredArgsConstructor
-@EnableConfigurationProperties({AdminInfoProperties.class})
+@EnableConfigurationProperties({ AdminInfoProperties.class })
 @SpringBootApplication
 public class ProjectBaseApplication {
 
-  private final UserRepository userRepository;
+	private final UserRepository userRepository;
 
-  private final RoleRepository roleRepository;
+	private final RoleRepository roleRepository;
 
-  private final PasswordEncoder passwordEncoder;
+	private final PasswordEncoder passwordEncoder;
 
+	public static void main(String[] args) {
+		Environment env = SpringApplication.run(ProjectBaseApplication.class, args).getEnvironment();
+		String appName = env.getProperty("spring.application.name");
+		if (appName != null) {
+			appName = appName.toUpperCase();
+		}
+		String port = env.getProperty("server.port");
+		log.info("-------------------------START " + appName + " Application------------------------------");
+		log.info("   Application         : " + appName);
+		log.info("   Url swagger-ui      : http://localhost:" + port + "/swagger-ui.html");
+		log.info("-------------------------START SUCCESS " + appName + " Application------------------------------");
+		String url = "http://localhost:8080/swagger-ui/index.html";
+		try {
+			if (isWindows()) {
+				new ProcessBuilder("cmd", "/c", "start", url).start();
+			} else if (isMac()) {
+				new ProcessBuilder("open", url).start();
+			} else if (isLinux()) {
+				new ProcessBuilder("xdg-open", url).start();
+			} else {
+				log.error("Hệ điều hành không được hỗ trợ, mở link thủ công: " + url);
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 
-  public static void main(String[] args) {
-    Environment env = SpringApplication.run(ProjectBaseApplication.class, args).getEnvironment();
-    String appName = env.getProperty("spring.application.name");
-    if (appName != null) {
-      appName = appName.toUpperCase();
-    }
-    String port = env.getProperty("server.port");
-    log.info("-------------------------START " + appName
-        + " Application------------------------------");
-    log.info("   Application         : " + appName);
-    log.info("   Url swagger-ui      : http://localhost:" + port + "/swagger-ui.html");
-    log.info("-------------------------START SUCCESS " + appName
-        + " Application------------------------------");
-  }
+	private static boolean isWindows() {
+		return System.getProperty("os.name").toLowerCase(Locale.ROOT).contains("win");
+	}
 
-  @Bean
-  CommandLineRunner init(AdminInfoProperties userInfo) {
-    return args -> {
-      //init role
-      if (roleRepository.count() == 0) {
-        roleRepository.save(new Role(null, RoleConstant.ADMIN, null));
-        roleRepository.save(new Role(null, RoleConstant.USER, null));
-      }
-      //init admin
-      if (userRepository.count() == 0) {
-        User admin = User.builder().username(userInfo.getUsername())
-            .password(passwordEncoder.encode(userInfo.getPassword()))
-            .firstName(userInfo.getFirstName()).lastName(userInfo.getLastName())
-            .role(roleRepository.findByRoleName(RoleConstant.ADMIN)).build();
-        userRepository.save(admin);
-      }
-    };
-  }
+	private static boolean isMac() {
+		return System.getProperty("os.name").toLowerCase(Locale.ROOT).contains("mac");
+	}
+
+	private static boolean isLinux() {
+		return System.getProperty("os.name").toLowerCase(Locale.ROOT).contains("nux");
+	}
+
+	@Bean
+	CommandLineRunner init(AdminInfoProperties userInfo) {
+		return args -> {
+			// init role
+			if (roleRepository.count() == 0) {
+				roleRepository.save(new Role(null, RoleConstant.ADMIN, null));
+				roleRepository.save(new Role(null, RoleConstant.USER, null));
+			}
+			// init admin
+			if (userRepository.count() == 0) {
+				User admin = User.builder().username(userInfo.getUsername())
+						.password(passwordEncoder.encode(userInfo.getPassword())).firstName(userInfo.getFirstName())
+						.lastName(userInfo.getLastName()).role(roleRepository.findByRoleName(RoleConstant.ADMIN))
+						.build();
+				userRepository.save(admin);
+			}
+		};
+	}
 
 }
--- BE/src/main/java/com/example/projectbase/controller/UserController.java
+++ BE/src/main/java/com/example/projectbase/controller/UserController.java
@@ -26,14 +26,14 @@ public class UserController {
   private final UserService userService;
 
   @Tag(name = "user-controller-admin")
-  @Operation(summary = "API get user",hidden = true)
+  @Operation(summary = "API get user")
   @GetMapping(UrlConstant.User.GET_USER)
   public ResponseEntity<?> getUserById(@PathVariable String userId) {
     return VsResponseUtil.success(userService.getUserById(userId));
   }
 
   @Tags({@Tag(name = "user-controller-admin"), @Tag(name = "user-controller")})
-  @Operation(summary = "API get current user login",hidden = true)
+  @Operation(summary = "API get current user login")
   @GetMapping(UrlConstant.User.GET_CURRENT_USER)
   public ResponseEntity<?> getCurrentUser(@Parameter(name = "principal", hidden = true)
                                           @CurrentUser UserPrincipal principal) {
@@ -41,7 +41,7 @@ public class UserController {
   }
 
   @Tag(name = "user-controller-admin")
-  @Operation(summary = "API get all customer",hidden = true)
+  @Operation(summary = "API get all customer")
   @GetMapping(UrlConstant.User.GET_USERS)
   public ResponseEntity<?> getCustomers(@Valid @ParameterObject PaginationFullRequestDto requestDTO) {
     return VsResponseUtil.success(userService.getCustomers(requestDTO));
